e043587f62c633a7cf7760c8ccd2820f1cfeda26
Tom Zander <billard3000@air-von-tom.speedport.ip>
2023-05-03 14:40:17 +0200

    initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-container class="fill-height">
+    <v-responsive class="d-flex align-center text-center fill-height">
+
+      <div class="text-body-2 font-weight-light mb-n1">Welcome to</div>
+
+      <h1>Select</h1>
+      <v-select :items="items"></v-select>
+
+    </v-responsive>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    items: [
+            {
+              "name": "123",
+              "id": "1",
+              "title": "title 1"
+            },
+            {
+              "name": "456",
+              "id": "2",
+              "title": "title 2"
+            }
+          ]
+  })
+}
+  
+</script>
